0278348ddc6c74b8536622a2316dcb04e1934dba
Timur3 <chalimovt@yandex.ru>
2019-02-02 19:34:43 +0500

    first commit

---
+++ MPGES.LK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MPGES.LK
+//
+//  Created by Timur on 24/01/2019.
+//  Copyright © 2019 Timur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MPGES.LKTests/Pods-MPGES.LKTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MPGES_LKTests : NSObject
+@end
+@implementation PodsDummy_Pods_MPGES_LKTests
+@end
