2b454be2e87bb544baf1254c6f35af72edf3ea85
Timur3 <timon2006tevriz@mail.ru>
2020-01-14 21:57:05 +0500

    Account login and password recovery desinger

---
+++ MPGES.LK/Core/Services/ApiService.swift
@@ -0,0 +1,12 @@
+//
+//  ApiService.swift
+//  mpges.lk
+//
+//  Created by Timur on 14.01.2020.
+//  Copyright © 2020 ChalimovTimur. All rights reserved.
+//
+
+import Foundation
+class ApiService {
+    
+}
